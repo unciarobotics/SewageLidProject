d3792cebd629c62b4fd87c5971d00ec3287b47ee
Ashish Kumar <ashishmarch12@gmail.com>
2019-05-20 15:14:24 +0530

    additions

--- 3.SIM900_SR04_TEST2/3.SIM900_SR04_TEST2.ino
+++ 3.SIM900_SR04_TEST2/3.SIM900_SR04_TEST2.ino
@@ -1,5 +1,5 @@
 #include <SoftwareSerial.h>
-SoftwareSerial mySerial(6, 7); //10 to 5VT and 9 to 5VR
+SoftwareSerial mySerial(7, 6); //10 to 5VT and 9 to 5VR
 
 #define trigPin 9
 #define echoPin 10
@@ -23,27 +23,16 @@ void setup()
 
 
 void loop()
-{
+{ int GasSensor = analogRead(A6);
   int button = digitalRead(3);
   digitalWrite(trigPin,HIGH);
   delayMicroseconds(1000);
   digitalWrite(trigPin,LOW);
   duration = pulseIn(echoPin,HIGH);
   distance= (duration/2)/29.1;
-  Serial.print(distance);
-  Serial.println("cm");
-
+  Serial.print("Distance: ");Serial.print(distance);
+  Serial.print("cm");Serial.print("     Gas Level: ");Serial.println(GasSensor);
   
- /* if (Serial.available()>0)
- switch(Serial.read())
- {
-  case 's':
-  SendMessage();
-  break;
-  case 'r':
-  RecieveMessage();
-  break;
- }*/
  if (distance<20&&ctr<=30)
  {ctr++;}
  if (distance<20&&ctr<30)
@@ -54,6 +43,15 @@ void loop()
 if (button==HIGH)
  {delay(100);
   SendMessageb();
+  }
+
+ if(GasSensor>=600)
+ {SendMessagec();
+  while(GasSensor>550)
+  {
+  GasSensor = analogRead(A6);
+  Serial.println(GasSensor);
+  delay(100);}
   }
  
  if (mySerial.available()>0)
@@ -80,6 +78,18 @@ void SendMessageb()
   mySerial.println("AT+CMGS=\"+917018093520\"\r"); // Replace x with mobile number
   delay(1000);
   mySerial.println("Lid Opened");// The SMS text you want to send
+  delay(100);
+  mySerial.println((char)26);// ASCII code of CTRL+Z
+  delay(1000);
+}
+
+void SendMessagec()
+{
+  mySerial.println("AT+CMGF=1");  //Sets the GSM Module in Text Mode
+  delay(1000);  // Delay of 1000 milli seconds or 1 second
+  mySerial.println("AT+CMGS=\"+917018093520\"\r"); // Replace x with mobile number
+  delay(1000);
+  mySerial.println("The level of Poisnous gas is rising above normal");// The SMS text you want to send
   delay(100);
   mySerial.println((char)26);// ASCII code of CTRL+Z
   delay(1000);
